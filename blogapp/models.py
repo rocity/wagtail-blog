14ccfc6ab01b5d602b51968acbfacba7dac7c080
Kevin Kalis <kevin@swiftkind.com>
2017-03-19 21:25:13 +0800

    Add a main image to posts

--- blogapp/models.py
+++ blogapp/models.py
@@ -29,6 +29,13 @@ class BlogPage(Page):
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
 
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
         index.SearchField('body'),
