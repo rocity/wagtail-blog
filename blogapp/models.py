80233c117f1625d0339c4e702d2912d842cba536
Kevin Kalis <kevin@swiftkind.com>
2017-03-19 19:36:56 +0800

    Create BlogPage model

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,6 +1,9 @@
+from django.db import models
+
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailsearch import index
 
 
 class BlogIndexPage(Page):
@@ -8,4 +11,21 @@ class BlogIndexPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname='full')
+    ]
+
+
+class BlogPage(Page):
+    date = models.DateField("Post Date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname='full'),
     ]
